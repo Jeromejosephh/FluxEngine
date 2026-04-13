f5fab17b16513366ab6d769f1d58b48e12129c80
Jeromejosephh <jeromejoseph311@gmail.com>
2026-04-13 22:56:50 +1200

    fix: add Vercel origin to CORS defaults and PATCH to allowed methods

--- utils/config.py
+++ utils/config.py
@@ -27,6 +27,7 @@ class Settings(BaseSettings):
     ALLOWED_ORIGINS: List[str] = [
         "http://localhost:3000",
         "http://localhost:8000",
+        "https://flux-engine.vercel.app",
     ]
 
     # SendGrid settings
